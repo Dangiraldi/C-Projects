1e6d087999548dfe42db34b4b0290a00cdebc432
Dangiraldi <dangiraldi312@gmail.com>
2017-02-24 13:32:17 -0500

    Page Table Simulation Project

--- Main.cpp
+++ Main.cpp
@@ -1,40 +1,22 @@
 #include <iostream>
-#include "Priority.h"
 using namespace std;
 
-int main()
+void pageTable(unsigned int);
+
+int main(unsigned int address)
 {
-    int choice, item, priority;
-    Priority_Queue pq; 
-    do
-    {
-        cout<<"1.Enqueue\n";
-        cout<<"2.Dequeue\n";
-        cout<<"3.Display\n";
-        cout<<"4.Quit\n";
-        cout<<"Enter your choice : "; 
-        cin>>choice;
-        switch(choice)
-        {
-        case 1:
-            cout<<"Input the item value to be added in the queue : ";
-            cin>>item;
-            cout<<"Enter its priority : ";
-            cin>>priority;
-            pq.insert(item, priority);
-            break;
-        case 2:
-            pq.del();
-            break;
-        case 3:
-            pq.display();
-            break;
-        case 4:
-			break;
-        default :
-            cout<<"Wrong choice\n";
-        }
-    }
-    while(choice != 4);
-    return 0;
+	cout<<"Enter Address(32-bit): ";
+	cin>>address;
+	pageTable(address);
+
+	system("pause");
+	return 0;
+}
+
+void pageTable(unsigned int a)
+{
+	unsigned int pageNum,offset;
+	pageNum=a/4096; //4096 because 4KB
+	offset=a%4096;
+	cout<<"The address "<<a<<" contains: \nPage number = "<<pageNum<<"\nOffset = "<<offset<<endl;
 }
